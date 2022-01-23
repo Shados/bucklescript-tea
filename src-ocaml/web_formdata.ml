2d27b4249f4bd5332674894a77ed23c031d0a08f
Alexei Robyn <shados@shados.net>
2022-01-23 15:14:37 +1100

    Migrate from bs-platform to the rebranded rescript package

--- src-ocaml/web_formdata.ml
+++ src-ocaml/web_formdata.ml
@@ -1,10 +1,4 @@
-
-class type _formdata = object
-  method append : string -> string -> unit
-  (* method append_blob : string -> Web_blob.t -> string -> unit *)
-end [@bs]
+class type _formdata = object method  append : string -> string -> unit end
 type t = _formdata Js.t
-
-external create : unit -> t = "FormData" [@@bs.new]
-
-let append key value f = f##append key value
+external create : unit -> t = "FormData"[@@bs.new ]
+let append key value f = (f ## append) key value
